5de7c48086091588e309437aec40ef856d0825c4
CrisAlex <cads.14alex@outlook.com>
2023-11-21 22:04:23 -0500

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styles: [],
+})
+export class AppComponent {
+  title = 'built-in-control-flow';
+  isActive = 1;
+  docs = [
+    { title: 'Explore the Docs', link: 'https://angular.dev' },
+    { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+    { title: 'Angular Language Service', link: 'https://angular.dev/tools/language-service' },
+    { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
+  ];
+  option = 0;
+}
